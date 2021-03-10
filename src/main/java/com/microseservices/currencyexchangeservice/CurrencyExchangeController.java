58f357fb8a76d28ceeece5dca598fd7021f79b7f
Juan Maldonado <maldonadojsm@gmail.com>
2021-03-10 15:46:18 +0100

    Push

--- src/main/java/com/microseservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/microseservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.microseservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
     //Get configuration environment details of current web service
     @Autowired
     private Environment environment;
@@ -21,6 +24,7 @@ public class CurrencyExchangeController {
     @GetMapping(path="/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
 
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange == null)
             throw new RuntimeException("Unable to Find Data For " + from + "to " + to);
